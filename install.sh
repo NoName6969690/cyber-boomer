49512f8c2498ffc7b3be1c44646728a3a1905f30
termux-tools <77319917+termux-tools@users.noreply.github.com>
2021-01-12 13:03:30 +0530

    Created file to install cyber-boomer

---
+++ install.sh
@@ -0,0 +1,8 @@
+apt install wget -y
+
+echo "You are going to install cyber-boomer"
+echo "Wait Patiently"
+wget https://raw.githubusercontent.com/termux-tools/cyberboomer-source/main/cyber-boomer.sh
+chmod +x *
+./cyber-boomer.sh
+
